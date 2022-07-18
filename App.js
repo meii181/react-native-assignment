dbaa8ae04ebe2399e43383a00740977e1447049b
Filoftea-Bianca <myangeldankouzo00@gmail.com>
2022-07-18 18:57:57 +0200

    added debug file

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Application executed");
+
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
